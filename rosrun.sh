14621bce3a04c417a24d9d0969a2c6da74870837
sam051200 <sam1111165@gmail.com>
2023-10-23 17:59:02 +0800

    update rosrun

--- rosrun.sh
+++ rosrun.sh
@@ -1,2 +1,7 @@
 #=================================MANO 
-rosrun ORB_SLAM3 Mono /dpds/ORB_SLAM3/Vocabulary/ORBvoc.txt /dpds/ORB_SLAM3/Examples/ROS/mycam.yaml /image
+# rosrun ORB_SLAM3 Mono /dpds/ORB_SLAM3/Vocabulary/ORBvoc.txt /dpds/ORB_SLAM3/Examples/ROS/mycam.yaml /image
+
+#---mvs
+# rosrun ORB_SLAM3 Mono /dpds/ORB_SLAM3/Vocabulary/ORBvoc.txt /dpds/ORB_SLAM3/camera/mvs.yaml /image gray
+rosrun ORB_SLAM3 Mono /dpds/ORB_SLAM3/Vocabulary/ORBvoc.txt /dpds/ORB_SLAM3/camera/mvs.yaml /image_edge gray
+# rosrun ORB_SLAM3 Mono /dpds/ORB_SLAM3/Vocabulary/ORBvoc.txt /dpds/ORB_SLAM3/camera/mvs.yaml /image_rgb rgb
